347d1e551f073b7dba30e76dc48f9a1bb7dfa947
MillzyDev <millzyyt08@gmail.com>
2022-04-23 16:44:16 +0100

    aaaaaaaaaaaaaaaaaahhhhhhhhhhhhhhhhh

---
+++ MonkePatcher/Services/MonkePatcherService.cs
@@ -0,0 +1,18 @@
+﻿using QuestPatcher.Core;
+using System;
+
+namespace MonkePatcher.Services
+{
+    internal class MonkePatcherService : QuestPatcherService
+    {
+        public MonkePatcherService() : base(new UIPrompter())
+        {
+
+        }
+
+        protected override void ExitApplication()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MonkePatcher/UIPrompter.cs
@@ -0,0 +1,39 @@
+﻿using QuestPatcher.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace MonkePatcher
+{
+    internal class UIPrompter : IUserPrompter
+    {
+        public Task<bool> Prompt32Bit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> PromptAdbDisconnect(DisconnectionType type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> PromptAppNotInstalled()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> PromptPauseBeforeCompile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> PromptUnstrippedUnityUnavailable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PromptUpgradeFromOld()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
